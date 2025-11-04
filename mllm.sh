36707375f021c7d7b9057dec07c6acb4faededa4
NilVidalRafols <nilvidal98@gmail.com>
2025-11-04 18:43:01 +0100

    feat(llm): add maki llm command
    This command makes use of the python cli tool llm
    (https://github.com/simonw/llm) to provide llm generated text
    using maki concatenated content as the prompt. It also accepts
    some llm options.

---
+++ mllm.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+cached_stdin=$(cat)
+main_content=""
+system_content=""
+model=""
+next_is_system=0
+next_is_template=0
+next_is_system_template=0
+next_is_model=0
+no_stream=1
+
+cmd=(maki)
+s_cmd=(maki)
+extra_args=()
+pipe=0
+s_pipe=0
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+    shift
+
+    if [[ "$arg" == "-st" ]]; then
+        s_cmd+=(-t "$1")
+        [[ $1 == "-" ]] && s_pipe=1
+        shift
+
+    elif [[ "$arg" == "-s" ]]; then
+        s_cmd+=("$1")
+        [[ $1 == "-" ]] && s_pipe=1
+        shift
+
+    elif [[ "$arg" == "-t" ]]; then
+        cmd+=(-t "$1")
+        [[ $1 == "-" ]] && pipe=1
+        shift
+        
+    elif [[ "$arg" == "-m" ]]; then
+        extra_args+=(-m $1)
+        shift
+
+    elif [[ "$arg" == "--no-stream" ]]; then
+        extra_args+=("$arg")
+
+    else
+        cmd+=("$arg")
+        [[ "$arg" == "-" ]] && pipe=1
+    fi
+done
+
+# Execute the maki command for main and system contents
+if [[ $pipe -eq 1 ]]; then
+    main_content="$(printf '%s' "$cached_stdin" | "${cmd[@]}")"$'\n'
+else
+    main_content="$("${cmd[@]}")"$'\n'
+fi
+
+if [[ $s_pipe -eq 1 ]]; then
+    system_content="$(printf '%s' "$cached_stdin" | "${s_cmd[@]}")"$'\n'
+else
+    system_content="$("${s_cmd[@]}")"$'\n'
+fi
+
+# Trim trailing newlines
+main_content="${main_content%$'\n'}"
+system_content="${system_content%$'\n'}"
+
+# printf "maki-llm mapped to [ printf '%%s' <main content> | llm -s <system content> %s ]\n" "${extra_args[*]}"
+# printf 'MAIN:\n%s\n' "$main_content"
+# printf 'SYSTEM:\n%s\n' "$system_content"
+
+# Run the llm command
+cmd=(llm)
+[[ -n $system_content ]] && cmd+=(-s "$system_content")
+[[ -n $extra_args ]] && cmd+=("${extra_args[@]}")
+printf '%s' "$main_content" | "${cmd[@]}"
